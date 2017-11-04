e6eeab4a2f3371532d1b23128e533a1a0419c7e5
Preston C Doris <prestonc.doris@Prestons-MacBook-Pro-2.local>
2017-11-04 13:01:37 -0700

    Complete rename_file.py
    I completed the file rename_file.py which then renamed all of the files in the folder "prank"

--- Use Functions/rename_file.py
+++ Use Functions/rename_file.py
@@ -1,5 +1,19 @@
+import os
 
 def rename_files():
     #(1) get file names from a folder
-
+    file_list = os.listdir(r"/Users/prestonc.doris/Desktop/Web Development/Udacity/Full Stack Web Dev Nano/Use Functions/prank")
+    
+    current_dir = os.getcwd()
+    
+    os.chdir(r"/Users/prestonc.doris/Desktop/Web Development/Udacity/Full Stack Web Dev Nano/Use Functions/prank")
+    
     #(2) for each file, rename filename
+    for file_name in file_list:
+        print("Old file name - " + file_name)
+        print("New file name - " + file_name.translate(None, "0123456789"))
+        os.rename(file_name, file_name.translate(None, "0123456789"))
+
+    os.chdir(current_dir)
+
+rename_files()
