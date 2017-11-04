7983fd3290c5ec6e902d56fd1335078dbd1deae6
Preston C Doris <prestonc.doris@Prestons-MacBook-Pro-2.local>
2017-11-04 13:18:22 -0700

    Add secret message folder
    Adjust the "rename_file" file to rename the files in the "secret message" folder

--- Use Functions/rename_file.py
+++ Use Functions/rename_file.py
@@ -2,11 +2,11 @@ import os
 
 def rename_files():
     #(1) get file names from a folder
-    file_list = os.listdir(r"/Users/prestonc.doris/Desktop/Web Development/Udacity/Full Stack Web Dev Nano/Use Functions/prank")
+    file_list = os.listdir(r"/Users/prestonc.doris/Desktop/Web Development/Udacity/Full Stack Web Dev Nano/Use Functions/secret message")
     
     current_dir = os.getcwd()
     
-    os.chdir(r"/Users/prestonc.doris/Desktop/Web Development/Udacity/Full Stack Web Dev Nano/Use Functions/prank")
+    os.chdir(r"/Users/prestonc.doris/Desktop/Web Development/Udacity/Full Stack Web Dev Nano/Use Functions/secret message")
     
     #(2) for each file, rename filename
     for file_name in file_list:
