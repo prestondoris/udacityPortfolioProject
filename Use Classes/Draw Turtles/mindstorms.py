7dded2b4354a7eaed3bb8f48502241f63da0b43c
Preston C Doris <prestonc.doris@Prestons-MacBook-Pro-2.local>
2017-11-05 09:59:31 -0800

    Add refactoring of code
    Created seperate functions to create a square, circle, and triangle. Called those functions inside of "draw_shapes"

--- Use Classes/Draw Turtles/mindstorms.py
+++ Use Classes/Draw Turtles/mindstorms.py
@@ -3,29 +3,40 @@ import turtle
 def draw_shapes():
     window = turtle.Screen()
     window.bgcolor("red")
-    brad = turtle.Turtle()
-    brad.shape("turtle")
-    brad.color("yellow")
-    brad.speed(2)
-    i = 0
-    while (i<4):
-        brad.forward(100)
-        brad.right(90)
-        i+=1
 
-    rosie = turtle.Turtle()
-    rosie.shape("arrow")
-    rosie.color("blue")
-    rosie.circle(100)
-
-    luna = turtle.Turtle()
-    j=0
-    while (j<3):
-        luna.forward(100)
-        luna.right(120)
-        j+=1
-    
+    draw_square(100, "turtle", "yellow", 2)
+    draw_circle(100, "circle", "blue", 2)
+    draw_triangle(300, "triangle", "black", 2)
 
     window.exitonclick()
+    
+def draw_square(size, shape, color, speed):
+    newSquare = turtle.Turtle()
+    newSquare.shape(shape)
+    newSquare.color(color)
+    newSquare.speed(speed)
+    i = 0
+    while (i<4):
+        newSquare.forward(size)
+        newSquare.right(90)
+        i+=1
 
+def draw_circle(radius, shape, color, speed):
+    newCircle = turtle.Turtle()
+    newCircle.shape(shape)
+    newCircle.color(color)
+    newCircle.speed(speed)
+    newCircle.circle(radius)
+
+def draw_triangle(size, shape, color, speed):
+    newTriangle = turtle.Turtle()
+    newTriangle.shape(shape)
+    newTriangle.color(color)
+    newTriangle.speed(speed)
+    j=0
+    while (j<3):
+        newTriangle.backward(size)
+        newTriangle.right(120)
+        j+=1
+    
 draw_shapes()
