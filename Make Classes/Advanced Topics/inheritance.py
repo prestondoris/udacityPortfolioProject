610ace6e5ecd951f73a47d66e6f9d640bed5afe0
Preston C Doris <prestonc.doris@Prestons-MacBook-Pro-2.local>
2017-11-07 15:44:47 -0800

    Add show_info() method inside of class Parent
    Prints out the last name and eye color

--- Make Classes/Advanced Topics/inheritance.py
+++ Make Classes/Advanced Topics/inheritance.py
@@ -4,6 +4,10 @@ class Parent():
 		self.last_name = last_name
 		self.eye_color = eye_color
 
+	def show_info():
+		print "Last Name - " + self.last_name
+		print "Eye Color - " + self.eye_color
+
 class Child(Parent):
 	def __init__(self, last_name, eye_color, num_toys):
 		Parent.__init__(self, last_name, eye_color)
