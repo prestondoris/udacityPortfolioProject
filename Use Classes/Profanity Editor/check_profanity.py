92c9af14f773d737bb40be54220e185060cff115
Preston C Doris <prestonc.doris@Prestons-MacBook-Pro-2.local>
2017-11-07 10:43:05 -0800

    Add check_profanity function to check_profanity file

--- Use Classes/Profanity Editor/check_profanity.py
+++ Use Classes/Profanity Editor/check_profanity.py
@@ -1,7 +1,18 @@
+import urllib
+
 def read_text():
 	quotes = open("/Users/prestonc.doris/Desktop/Web Development/Udacity/Full Stack Web Dev Nano/Use Classes/Profanity Editor/movie_quotes.txt")
 	contents = quotes.read()
 	print(contents)
 	quotes.close()
+	check_profanity(contents)
+
+def check_profanity(text_to_check):
+	#urlopen() works just like the open() function except you
+	#supply a url instead of the local file path
+	connection  = urllib.urlopen("http://www.wdylike.appspot.com/?q=" + text_to_check)
+	output = connection.read()
+	print output
+	connection.close()
 
 read_text()
