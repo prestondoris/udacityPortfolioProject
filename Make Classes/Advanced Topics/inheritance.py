a51cc63a8225e41be1104d2cc869c4917f8847ca
Preston C Doris <prestonc.doris@Prestons-MacBook-Pro-2.local>
2017-11-07 15:32:38 -0800

    Test Parent class working correctly

--- Make Classes/Advanced Topics/inheritance.py
+++ Make Classes/Advanced Topics/inheritance.py
@@ -4,7 +4,5 @@ class Parent():
 		self.last_name = last_name
 		self.eye_color = eye_color
 
-class Child(Parent):
-	def __init__(self, last_name, eye_color, num_toys):
-		Parent.__init__(self, last_name, eye_color)
-		self.num_toys = num_toys
+jenna_doris = Parent("Doris", "Blue-Green")
+print(jenna_doris.last_name)
