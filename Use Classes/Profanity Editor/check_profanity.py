8031035da68a02d8d14c3046d58fc2a0ea829b4c
Preston C Doris <prestonc.doris@Prestons-MacBook-Pro-2.local>
2017-11-07 10:55:27 -0800

    Update "check_profanity.py"
    Added clearer outputs using if statements

--- Use Classes/Profanity Editor/check_profanity.py
+++ Use Classes/Profanity Editor/check_profanity.py
@@ -3,7 +3,6 @@ import urllib
 def read_text():
 	quotes = open("/Users/prestonc.doris/Desktop/Web Development/Udacity/Full Stack Web Dev Nano/Use Classes/Profanity Editor/movie_quotes.txt")
 	contents = quotes.read()
-	print(contents)
 	quotes.close()
 	check_profanity(contents)
 
@@ -12,7 +11,12 @@ def check_profanity(text_to_check):
 	#supply a url instead of the local file path
 	connection  = urllib.urlopen("http://www.wdylike.appspot.com/?q=" + text_to_check)
 	output = connection.read()
-	print output
+	if "true" in output:
+		print "Profanity Alert! You have a profane word in your document."
+	elif "false" in output:
+		print "There are no profane words in your document."
+	else:
+		print "There was a problem scanning your document."
 	connection.close()
 
 read_text()
