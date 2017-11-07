c559699912f1f821794041c0ba184c657fe167e0
Preston C Doris <prestonc.doris@Prestons-MacBook-Pro-2.local>
2017-11-07 15:28:36 -0800

    Add child class to "inheritance.py"
    This class inherts the Parent class last_name and eye_color while accepting a num_toys argument

--- Make Classes/Advanced Topics/inheritance.py
+++ Make Classes/Advanced Topics/inheritance.py
@@ -4,3 +4,7 @@ class Parent():
 		self.last_name = last_name
 		self.eye_color = eye_color
 
+class Child(Parent):
+	def __init__(self, last_name, eye_color, num_toys):
+		Parent.__init__(self, last_name, eye_color)
+		self.num_toys = num_toys
