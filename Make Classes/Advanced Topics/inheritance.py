d1a64079bdf10859aff2cc0aff6480394c9d0c6f
Preston C Doris <prestonc.doris@Prestons-MacBook-Pro-2.local>
2017-11-07 15:53:50 -0800

    Add spacing after class Parent

--- Make Classes/Advanced Topics/inheritance.py
+++ Make Classes/Advanced Topics/inheritance.py
@@ -7,6 +7,7 @@ class Parent():
 	def show_info():
 		print "Last Name - " + self.last_name
 		print "Eye Color - " + self.eye_color
+		
 
 class Child(Parent):
 	def __init__(self, last_name, eye_color, num_toys):
