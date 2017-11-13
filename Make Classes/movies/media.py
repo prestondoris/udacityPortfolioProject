8320b14915d20e78eda763ce7eb920b2ff79267c
Preston C Doris <prestonc.doris@Prestons-MacBook-Pro-2.local>
2017-11-12 17:55:10 -0800

    Update __.doc__ in media.py

--- Make Classes/movies/media.py
+++ Make Classes/movies/media.py
@@ -1,7 +1,11 @@
 import webbrowser
 
-class Movie(): 
-    """ The class provides a way to store movie related information """
+class Movie():
+    """
+    The class Movie provides a way to store the Title, brief
+    story line information, a link to a movie poster image,
+    and a link to the movie trailer on youtube.
+    """
     def __init__(self, movie_title, movie_storyline,
                  poster_image, trailer_youtube):
         self.title = movie_title
